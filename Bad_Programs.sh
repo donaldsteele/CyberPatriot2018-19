fc3fd0e4105557c9aa95bda6a1fda246dbaa660a
Phishers <34041366+Phishers@users.noreply.github.com>
2018-09-13 15:07:52 -0400

    Create Bad_Programs.sh

---
+++ Bad_Programs.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+echo "This script will uninstall bad programs!"
+
+sudo apt-get -y purge pure-ftpd*
+
+sudo apt-get -y purge ftp*
+
+sudo apt-get -y purge samba*
+
+sudo apt-get -y purge nmap*
+
+sudo apt-get -y purge zenmap*
+
+sudo apt-get -y purge hydra*
+
+sudo apt-get -y purge john*
+
+sudo apt-get -y purge nikto*
+
+sudo apt-get -y purge netcat*
+
+sudo apt-get -y purge wireshark*
+
+sudo apt-get -y purge weplab*
+
+sudo apt-get -y purge pyrit*
+
+sudo apt-get -y purge ophcrack*
+
+echo "Done uninstalling bad programs!"
